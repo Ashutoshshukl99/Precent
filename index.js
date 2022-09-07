f67a97e631a5289619aabf9ff16346679560206d
Ashutosh shukla <91814968+Ashutoshshukl99@users.noreply.github.com>
2022-09-07 10:48:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ function calc(){
          let input1 = document.getElementById("inp1").value;
          let input2 = document.getElementById("inp2").value;
          let res = (input1*input2)/100;
+         let resLine = input2+"% of "+input1+" is = " +res;
          document.getElementById("result").innerHTML = res;
+         document.getElementById("result-line").innerHTML = resLine;
        }
